1a46c52b95ca1735e4023c13aaed911871c20abc
gostisa97 <bamo97@hotmail.com>
2021-07-29 11:37:20 +0200

    1 file modified, 1 added

---
+++ src/app/transportation.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { Car } from './car';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransportationService {
+
+   // this is where we will recreate our array of Car types
+
+
+  constructor() { }
+
+  // this is where we will write methods to export our Car array
+}
